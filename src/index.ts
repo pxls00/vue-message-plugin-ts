825c2698fe7fb63ea4f47c492fedc42bf42a3fab
Abdurahim <89141756+pxls00@users.noreply.github.com>
2023-03-28 07:46:50 +0500

    configuration for publish to npmjs

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import MessagesList from '@/components/messages-list.vue'
+import { createPinia } from 'pinia'
+import MessagePlugin from '@/plugins/message'
+
+export default {
+  install: (app: any) => {
+    app.component('AppMessages', MessagesList)
+    const pinia = createPinia()
+
+    app.use(pinia)
+    app.use(MessagePlugin)
+  }
+}
