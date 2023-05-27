234b298e3b86ad92abbce023714bdfbbf91c3411
Abdurahim <89141756+pxls00@users.noreply.github.com>
2023-05-27 22:18:56 +0500

    integration pinia successfully

--- src/components/messages-list.spec.ts
+++ src/components/messages-list.spec.ts
@@ -2,11 +2,13 @@ import {mount, type VueWrapper, type DOMWrapper} from '@vue/test-utils'
 import { useMessagesStore } from '@/stores/messages'
 import { createTestingPinia } from '@pinia/testing'
 import { nextTick } from 'vue'
+import { createPinia } from 'pinia';
 
 import MessagesList from '@/components/messages-list.vue'
 import AppMessageComponent from '@/components/ui/app-message.vue'
 import type MessageItem from '@/interfaces/messages/message-item'
 import DefaultMessageWaitData from '@/constants/message-wait'
+import { doesNotMatch, doesNotReject } from 'assert';
 
 const messageList: MessageItem[] = [
   {
@@ -39,17 +41,22 @@ const messageList: MessageItem[] = [
 
 describe('Messages List Component:', () => {
   let wrapper: VueWrapper
-  let store : any
+  let pinia : any
 
-  beforeAll(() => {
+  beforeEach(() => {
+    pinia = createPinia()
     wrapper = mount(MessagesList, {
       global: {
-        plugins:[createTestingPinia()]
+        plugins:[pinia]
       }
     })
-    store = useMessagesStore()
-    store.$patch({messages: messageList})
-    store.waitAction()
+    const messageStore = useMessagesStore(pinia)
+
+    messageStore.$state.isWait = false
+    messageStore.$state.messages = []
+
+    messageStore.$state.messages = messageList
+    messageStore.waitAction()
   })
 
   it('check every message items renderable paramters is contains in html', () => {
@@ -74,12 +81,15 @@ describe('Messages List Component:', () => {
           }
         }
       })
+
       if(itemEl.attributes('data-message-type') === 'wait') {
         const messageItemImgBlock = itemEl.find(`[data-testid="message__img-block-wait"]`)
         const messageItemImg = itemEl.find(`[data-testid="message__img-custom"]`)
         const messageItemTitle = itemEl.find(`[data-testid="message__title-content"]`)
 
-        expect(itemEl.classes()).toEqual(DefaultMessageWaitData.class)
+        if(DefaultMessageWaitData.class && DefaultMessageWaitData.class.length) {
+          expect(itemEl.classes()).toEqual(DefaultMessageWaitData.class)
+        }
         expect(messageItemTitle.exists()).toBe(true)
         expect(messageItemTitle.html()).toContain(DefaultMessageWaitData.title)
 
@@ -93,24 +103,57 @@ describe('Messages List Component:', () => {
       }
     })
   })
-  it('check is remove message is works', async () => {
+
+  it('check is remove message is working', async () => {
     const messageErrorItem = messageList.find(item => item.type === 'error') as MessageItem
     const messageItem = wrapper.find(`[data-testid="message__item"][data-message-type="${messageErrorItem.type}"]`)
-
-    const removeMessageMutation = jest.fn()
     
-    store.removeMessage = removeMessageMutation
+    const messageStore = useMessagesStore()
+    const messageStoreRemoveMessageMutation = messageStore.removeMessage
+    messageStore.removeMessage = jest.fn((...args) => {
+      messageStoreRemoveMessageMutation.apply(messageStore, args)
+    })
 
     const messageRemoveBtn = messageItem.find('[data-testid="message__close-button"]')
 
     messageRemoveBtn.trigger('click')
 
-    expect(removeMessageMutation).toHaveBeenCalled()
-    expect(store.messages.includes(messageErrorItem)).not.toBe(true)
+    expect(messageStore.removeMessage).toHaveBeenCalledTimes(1)
+    expect(messageStore.removeMessage).toHaveBeenCalledWith(messageErrorItem)
+    
+    await nextTick()
+    
+    expect(messageStore.$state.messages.includes(messageErrorItem)).not.toBe(true)
+    // expect(messageStore.$state.messages.filter(item => item.type === 'error')).toBe([])
+    expect(wrapper.html()).not.toContain(messageErrorItem.title)
+  })
 
+  it('check is adding new message is working', async () => {
+    const newMessage = JSON.parse(JSON.stringify(messageList.find(item => item.type === 'success'))) as MessageItem
+    newMessage.id = 123
+    newMessage.duration = 2000
+    newMessage.title = 'Test success message remove after 2 seconds'
+    
+    
+    const messageStore = useMessagesStore()
+    messageStore.newMessage(newMessage)
+    
     await nextTick()
 
-    expect(wrapper.html()).not.toContain(messageErrorItem.title)
-    // await expect(wrapper.html()).not.toContain(messageErrorItem.title)
+    const messageItem = wrapper.findAll(`[data-testid="message__item"][data-message-type="${newMessage.type}"]`)
+
+    expect(messageItem.length).toBe(2)
+    expect(messageItem[1].text()).toContain(newMessage.title)
+
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(null)
+      }, newMessage.duration)
+    })
+
+    const newMesssageItem = wrapper.findAll(`[data-testid="message__item"][data-message-type="${newMessage.type}"]`)
+    expect(newMesssageItem.length).toBe(1)
+    expect(messageStore.messages.filter(item => item.type === newMessage.type).length).toBe(1)
   })
+
 })
